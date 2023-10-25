8e218942e16c800fa175ab2f2a3ebd4e2dd5927a
iamayushgupta <ayushgupta71011@gmail.com>
2023-10-25 15:39:57 +0530

    Created routes and controllers for session and cookies

--- controller/authController.go
+++ controller/authController.go
@@ -6,8 +6,10 @@ import (
 	"github.com/ayush/go-auth/config"
 	"log" 
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/sessions"
 )
 
+//Basic-Auth Signup
 func SignUp(c *gin.Context) {
 	var user model.User
 	if err := c.BindJSON(&user); err != nil {
@@ -27,6 +29,8 @@ func SignUp(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "signup successful"})
 }
 
+
+//Basic-Auth Login
 func Login(c *gin.Context) {
 	var user model.User
 	if err := c.BindJSON(&user); err != nil {
@@ -44,4 +48,56 @@ func Login(c *gin.Context) {
 
 	log.Printf("User %s logged in successfully", user.Username)
 	c.JSON(http.StatusOK, gin.H{"message": "login successful"})
+}
+
+
+var store = sessions.NewCookieStore([]byte("secret"))
+
+//Session Login
+func SessionLogin(c *gin.Context) {
+	var credentials model.User
+	if err := c.BindJSON(&credentials); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Cannot bind JSON input"})
+		return
+	}
+
+	if err := credentials.Login(config.DB); err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Incorrect username or password"})
+		return
+	}
+
+	session, _ := store.Get(c.Request, "mysession")
+	session.Values["username"] = credentials.Username
+	if err := session.Save(c.Request, c.Writer); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save session"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Login successful"})
+}
+
+//Session Dashboard
+func Dashboard(c *gin.Context) {
+	session, _ := store.Get(c.Request, "mysession")
+	username := session.Values["username"]
+	c.JSON(http.StatusOK, gin.H{"message": "Welcome to the dashboard", "user": username})
+}
+
+//Session Logout
+func SessionLogout(c *gin.Context) {
+	session, _ := store.Get(c.Request, "mysession")
+	session.Values["username"] = nil
+	session.Save(c.Request, c.Writer)
+	c.JSON(http.StatusOK, gin.H{"message": "Logged out successfully"})
+}
+
+//Session Auth
+func AuthRequired(c *gin.Context) {
+	session, _ := store.Get(c.Request, "mysession")
+	if session.Values["username"] == nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		c.Abort()
+		return
+	}
+	c.Next()
 }
--- router/router.go
+++ router/router.go
@@ -9,5 +9,8 @@ func SetupRouter() *gin.Engine {
 	r := gin.Default()
 	r.POST("/signup", controller.SignUp)
 	r.POST("/login", controller.Login)
+	r.POST("/session/login", controller.SessionLogin)
+	r.GET("/session/dashboard", controller.AuthRequired, controller.Dashboard)
+	r.GET("/session/logout", controller.SessionLogout)
 	return r
 }
