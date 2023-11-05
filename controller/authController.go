6360e22976f8a386d5b784225b7cadcf737f304b
iamayushgupta <ayushgupta71011@gmail.com>
2023-11-06 01:11:57 +0530

    created constants for errors

--- controller/authController.go
+++ controller/authController.go
@@ -8,18 +8,25 @@ import (
 	"net/http"
 )
 
-// Making constants for error message
+const (
+	invalidInputError      = "Invalid input"
+	internalServerError    = "Internal Server Error"
+	usernameExistsError    = "Username already exists"
+	usernameNotFoundError  = "Username does not exist"
+	incorrectPasswordError = "Password is incorrect"
+	tokenGenerationError   = "Could not generate a Token"
+)
+
 func SignUp(c *gin.Context, userService *service.UserService) {
 	var user model.User
 	if err := c.BindJSON(&user); err != nil {
-		log.Printf("Failed to bind JSON input for signup: %v", err)
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		handleError(c, http.StatusBadRequest, invalidInputError)
 		return
 	}
 
 	statusCode, err := userService.SignUp(&user)
 	if err != nil {
-		c.JSON(statusCode, gin.H{"error": err.Error()})
+		handleError(c, statusCode, err.Error())
 		return
 	}
 
@@ -27,19 +34,16 @@ func SignUp(c *gin.Context, userService *service.UserService) {
 	c.JSON(statusCode, gin.H{"message": "signup successful"})
 }
 
-// Basic-Auth Login
 func Login(c *gin.Context, userService *service.UserService) {
 	var user model.User
 	if err := c.ShouldBindJSON(&user); err != nil {
-		log.Printf("Failed to bind JSON input for signup: %v", err)
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		handleError(c, http.StatusBadRequest, invalidInputError)
 		return
 	}
 
 	statusCode, err := userService.Login(&user)
-
 	if err != nil {
-		c.JSON(statusCode, gin.H{"error": err.Error()})
+		handleError(c, statusCode, err.Error())
 		return
 	}
 
@@ -47,24 +51,28 @@ func Login(c *gin.Context, userService *service.UserService) {
 	c.JSON(statusCode, gin.H{"message": "Login successful"})
 }
 
-// Login using JWT
 func JwtLogin(c *gin.Context, userService *service.UserService) {
 	var user model.User
 	if err := c.BindJSON(&user); err != nil {
-		log.Printf("Failed to bind JSON input for signup: %v", err)
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		handleError(c, http.StatusBadRequest, invalidInputError)
 		return
 	}
 
 	statusCode, token, err := userService.JwtLogin(&user)
 	if err != nil {
-		c.JSON(statusCode, gin.H{"error": err.Error()})
+		handleError(c, statusCode, err.Error())
 		return
 	}
+
 	log.Printf("Token Generated Successfully for user %s", user.Username)
 	c.JSON(http.StatusOK, gin.H{"token": token})
 }
 
 func SecureEndpoint(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "You are authenticated"})
+}
+
+func handleError(c *gin.Context, statusCode int, message string) {
+	log.Printf("Error: %s", message)
+	c.JSON(statusCode, gin.H{"error": message})
 }
